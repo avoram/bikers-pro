c2c47bf61bb08955259891505c8c6179156f8454
avoram <mayank.vora@snapav.com>
2016-07-20 03:31:56 +0530

    Bikers Pro MEAN Flow working

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bikers-pro
 Bikers pro work
+
+Running MongoDb Server :
+
+1) Go to bin folder
+2) run ./mongod : It will start mongo db server at port no : 27017
+3) Now run ./mongo in new terminal: We can now use it to create or modify the database and collection
---
+++ server/models/service-providers-model.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+//Here model has 3 arguments : 3rd argument is the name of the collection (serviceProviders)
+//http://stackoverflow.com/questions/14183611/mongoose-always-returning-an-empty-array-nodejs
+
+module.exports = mongoose.model("Model", {
+  name: String
+}, "serviceProviders");
