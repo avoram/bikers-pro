0baec9fd292bb5d329fae68f7cf3fa3551af0fbf
avoram <mayank.vora@snapav.com>
2016-07-31 03:44:28 +0530

    This work

--- server/controllers/service-provider-controller.js
+++ server/controllers/service-provider-controller.js
@@ -1,7 +1,7 @@
 //var ServiceProvider = require('../models/service-providers-model');
 
 module.exports.serviceProvidersList = function(req, res) {
-  console.log('In Controller of server');
+  console.log('In Controller of My Server finally');
 //  ServiceProvider.find({}, function(err, result) {
   //  res.json(result);
 //  });
