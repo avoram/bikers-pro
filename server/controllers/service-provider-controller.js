6051c3a3f707d9222d040a66069e0c174a1fd0b7
avoram <mayank.vora@snapav.com>
2016-07-31 03:40:58 +0530

    New one

--- server/controllers/service-provider-controller.js
+++ server/controllers/service-provider-controller.js
@@ -2,7 +2,6 @@
 
 module.exports.serviceProvidersList = function(req, res) {
   console.log('In Controller of server');
-  res.send('Mayank Vora Here');
 //  ServiceProvider.find({}, function(err, result) {
   //  res.json(result);
 //  });
