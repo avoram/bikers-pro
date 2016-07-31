5d23d756d067c1106ea8b5d51ae3e6a89ddb4dfa
avoram <mayank.vora@snapav.com>
2016-07-31 12:50:49 +0530

    CORS Issue

--- server/controllers/service-provider-controller.js
+++ server/controllers/service-provider-controller.js
@@ -1,6 +1,8 @@
 var ServiceProvider = require('../models/service-providers-model');
 
 module.exports.serviceProvidersList = function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   ServiceProvider.find({}, function(err, result) {
     res.json(result);
   });
--- www/js/controllers/service-providers-controller.js
+++ www/js/controllers/service-providers-controller.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers')
+  angular.module('app.controllers')
 .controller('serviceProvidersCtrl', function($scope,$resource,$http) {
   var ServiceProvider = $resource('https://bikers-pro.herokuapp.com/api/serviceProvider',{q: '*'},{'query': { method: 'GET',isArray: true}});
   ServiceProvider.query(function(results){
